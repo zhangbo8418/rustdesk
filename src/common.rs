254023ca6174378eb9a49c2437851e1186640cd1
裙下孤魂 <46797568+zhangbo8418@users.noreply.github.com>
2024-06-25 13:01:21 +0800

    Update common.rs

--- src/common.rs
+++ src/common.rs
@@ -1058,12 +1058,12 @@ pub fn get_api_server(api: String, custom: String) -> String {
             return format!("http://{}", s);
         }
     }
-    "https://admin.rustdesk.com".to_owned()
+    "https://rd.bobohome.store:8415".to_owned()
 }
 
 pub fn get_audit_server(api: String, custom: String, typ: String) -> String {
     let url = get_api_server(api, custom);
-    if url.is_empty() || url.contains("rustdesk.com") {
+    if url.is_empty() || url.contains("bbf.x3322.net:8415") {
         return "".to_owned();
     }
     format!("{}/api/audit/{}", url, typ)
