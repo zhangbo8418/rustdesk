173428ae64e3762a354dc1ab9e65d7d9c9aa3551
裙下孤魂 <46797568+zhangbo8418@users.noreply.github.com>
2024-10-24 23:20:21 +0800

    Update config.rs

--- libs/hbb_common/src/config.rs
+++ libs/hbb_common/src/config.rs
@@ -100,8 +100,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["www.gqysh.com"];
-pub const PUBLIC_RS_PUB_KEY: &str = "RjjvtcOkXf4jf+JxJSRT3kqV0W2npkSK8R8pUpQLEw4=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["bobohome.ignorelist.com","rd.bobohome.store"];
+pub const PUBLIC_RS_PUB_KEY: &str = "qwRw4z2gakEG5YQEVQTVqxlzryONTJqOAuoyCK3Lyzc=";
 
 pub const RS_PUB_KEY: &str = match option_env!("RS_PUB_KEY") {
     Some(key) if !key.is_empty() => key,
