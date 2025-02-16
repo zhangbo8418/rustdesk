c20de085a1efc7793a5481eb6a8f0133e785a39a
Adwin White <adwinw01@gmail.com>
2025-02-16 20:43:00 +0800

    feat(ui): handle camera connection type in flutter

--- flutter/lib/common.dart
+++ flutter/lib/common.dart
@@ -98,6 +98,7 @@ enum DesktopType {
   main,
   remote,
   fileTransfer,
+  viewCamera,
   cm,
   portForward,
 }
@@ -2198,7 +2199,7 @@ bool handleUriLink({List<String>? cmdArgs, Uri? uri, String? uriString}) {
 List<String>? urlLinkToCmdArgs(Uri uri) {
   String? command;
   String? id;
-  final options = ["connect", "play", "file-transfer", "port-forward", "rdp"];
+  final options = ["connect", "play", "file-transfer", "view-camera", "port-forward", "rdp"];
   if (uri.authority.isEmpty &&
       uri.path.split('').every((char) => char == '/')) {
     return [];
@@ -2236,6 +2237,8 @@ List<String>? urlLinkToCmdArgs(Uri uri) {
         connect(Get.context!, id);
       } else if (optionIndex == 2) {
         connect(Get.context!, id, isFileTransfer: true);
+      } else if (optionIndex == 3) {
+        connect(Get.context!, id, isViewCamera: true);
       }
       return null;
     }
@@ -2288,6 +2291,7 @@ List<String>? urlLinkToCmdArgs(Uri uri) {
 
 connectMainDesktop(String id,
     {required bool isFileTransfer,
+    required bool isViewCamera,
     required bool isTcpTunneling,
     required bool isRDP,
     bool? forceRelay,
@@ -2299,6 +2303,11 @@ connectMainDesktop(String id,
         password: password,
         isSharedPassword: isSharedPassword,
         connToken: connToken,
+        forceRelay: forceRelay);
+  } else if (isViewCamera) {
+    await rustDeskWinManager.newViewCamera(id,
+        password: password,
+        isSharedPassword: isSharedPassword,
         forceRelay: forceRelay);
   } else if (isTcpTunneling || isRDP) {
     await rustDeskWinManager.newPortForward(id, isRDP,
@@ -2316,10 +2325,12 @@ connectMainDesktop(String id,
 
 /// Connect to a peer with [id].
 /// If [isFileTransfer], starts a session only for file transfer.
+/// If [isViewCamera], starts a session only for view camera.
 /// If [isTcpTunneling], starts a session only for tcp tunneling.
 /// If [isRDP], starts a session only for rdp.
 connect(BuildContext context, String id,
     {bool isFileTransfer = false,
+    bool isViewCamera = false,
     bool isTcpTunneling = false,
     bool isRDP = false,
     bool forceRelay = false,
@@ -2351,6 +2362,7 @@ connect(BuildContext context, String id,
       await connectMainDesktop(
         id,
         isFileTransfer: isFileTransfer,
+        isViewCamera: isViewCamera,
         isTcpTunneling: isTcpTunneling,
         isRDP: isRDP,
         password: password,
@@ -2361,6 +2373,7 @@ connect(BuildContext context, String id,
       await rustDeskWinManager.call(WindowType.Main, kWindowConnect, {
         'id': id,
         'isFileTransfer': isFileTransfer,
+        'isViewCamera': isViewCamera,
         'isTcpTunneling': isTcpTunneling,
         'isRDP': isRDP,
         'password': password,
@@ -2701,6 +2714,8 @@ String getWindowName({WindowType? overrideType}) {
       return name;
     case WindowType.FileTransfer:
       return "File Transfer - $name";
+    case WindowType.ViewCamera:
+      return "View Camera - $name";
     case WindowType.PortForward:
       return "Port Forward - $name";
     case WindowType.RemoteDesktop:
